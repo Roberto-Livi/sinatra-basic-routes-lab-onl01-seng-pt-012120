82df4f13df2a27ac343293907d775897c8822fde
Roberto Livi <robertomlivi@outlook.com>
2020-03-29 04:23:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,16 @@ class App < Sinatra::Base
     "Hello, World!"
   end
   
-  get
+  get '/name' do
+    "My name is _"
+  end
+  
+  get '/hometown' do
+    "My hometown is _"
+  end
+  
+  get '/favorite_song' do
+    "My favorite song is _"
+  end
   
 end
